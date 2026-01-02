1134c8bcf11279df25a27f2917f1a5aab048a01f
rpbotforg-boop <rpbotforg@gmail.com>
2026-01-02 14:52:57 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -13,11 +13,11 @@ DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://niravpatel180503_db
 MONGO_URL = DATABASE_URL  # For auth system
 
 # Owner and Admin Configuration
-OWNER_ID = int(os.environ.get("OWNER_ID", "6201066540"))
-ADMINS = [int(x) for x in os.environ.get("ADMINS", "6201066540").split()]  # Default to owner ID
+OWNER_ID = int(os.environ.get("OWNER_ID", "777756062"))
+ADMINS = [int(x) for x in os.environ.get("ADMINS", "777756062").split()]  # Default to owner ID
 
 # Channel Configuration
-PREMIUM_CHANNEL = "https://t.me/+zzgp8DUgaic3MzRl"
+PREMIUM_CHANNEL = "https://t.me/+qTv8z74iwss1Y2U1"
 # Thumbnail Configuration
 THUMBNAILS = list(map(str, os.environ.get("THUMBNAILS", "https://files.catbox.moe/fh731v.jpg").split())) # Image Link For Default Thumbnail 
 
@@ -59,6 +59,7 @@ Please contact the admin @ItsUGBot to get access.</blockquote>""",
 
 <blockquote>Use format: {format}</blockquote>"""
 }
+
 
 
 
