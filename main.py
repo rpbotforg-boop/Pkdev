c617a5c288f8959bb598aed9f9cc14ea7a7425a3
rpbotforg-boop <rpbotforg@gmail.com>
2026-01-07 23:13:09 +0530

    Update API endpoint for classplusapp

--- main.py
+++ main.py
@@ -792,7 +792,7 @@ async def txt_handler(bot: Client, m: Message):
                 keys_string = " ".join([f"--key {key}" for key in keys])
 
             elif "classplusapp" in url:
-                signed_api = f"https://itsgolu-cp-api.vercel.app/itsgolu?url={url_norm}@ITSGOLU_OFFICIAL&user_id=6201066540"
+                signed_api = f"https://covercel.vercel.app/extract_keys?url={url}@bots_updatee&user_id={user_id}"
                 response = requests.get(signed_api, timeout=40)
                 url = response.text.strip()
                 url = response.json()['url']  
